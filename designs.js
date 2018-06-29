bf2c717f894604c58a6d49f50445ff2d698b26b1
tombra4ril <tombra4ril@gmail.com>
2018-06-28 17:44:41 -0700

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,45 @@
+// Select color input
+const COLOUR = $("#colorPicker");
+
+// Select size input
+const HEIGHT_ELEMENT = $("#inputHeight");
+const WIDTH_ELEMENT = $("#inputWeight");
+
+// Select table element
+const TABLE_ELEMENT = $("#pixelCanvas");
+
+// Select form element
+const SUBMIT = $("#sizePicker");
+
+// Create variables to hold the values to pass to the function
+var width, height;
+
+// Add an event listener to form when size is submitted by the user, call makeGrid()
+SUBMIT.on("submit", function(event){
+							width = WIDTH_ELEMENT.val();
+							height = HEIGHT_ELEMENT.val();
+							WIDTH_ELEMENT.val("1");
+							HEIGHT_ELEMENT.val("1");
+							event.preventDefault();
+							makeGrid();
+						});
+
+function makeGrid(){
+// Your code goes here!
+// Make the grid
+	let grid = "";
+	let start = 0;
+	while(start++ <= height){
+		grid += "<tr>";
+		for(let count = 1; count <= width; count++){
+			grid += "<td class='click'></td>";
+		}
+		grid += "</tr>";
+	}
+
+	TABLE_ELEMENT.html(grid);
+	
+// Add event listener to each <td> element and set the colour
+	let tdElements = $(".click");
+	tdElements.on("click", function(){$(this).css("background-color", COLOUR.val())});
+}
